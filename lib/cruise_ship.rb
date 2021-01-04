178d0a81c6a4146ea3bf81767d2ae419f9e6e340
Erik Huang <erikhuangdrums@gmail.com>
2021-01-04 21:11:49 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,9 @@
 # }
 
 def #select_winner(hash)
-  hash.each { |k, v| return v if (v[0] == "A") && (k == :suite_a) }
+  hash.each do |k, v|
+    if (k == :suite_a) && (v[0] == "A")
+      return v
+    end
+  end
 end
